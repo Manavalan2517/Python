7ffbc4d06195ed8607338b8210755d001210a9c7
Mano <vmanavalan2517@outlook.in>
2024-01-15 16:34:57 +0530

    Update Problem_26.py

--- Python_Practice_Questions/P1_Basic_Python/Problem_26.py
+++ Python_Practice_Questions/P1_Basic_Python/Problem_26.py
@@ -5,4 +5,5 @@
 
 num1 = ("Enter number 1: ")
 num2 = ("Enter number 2: ")
-print("Sum of two numbers = ", (num1 + num2))
+num3 = ("Enter number 3: ")
+print("Sum of two numbers = ", (num1 + num2 + num3))
